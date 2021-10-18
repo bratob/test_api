e422d6f65cc6bb04bd88018f4a6fb54cfc760e80
bratob <50356931+bratob@users.noreply.github.com>
2021-10-18 10:57:17 +0200

    updated to iot

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Dresden&appid=8fd2d96a373a341230be93f9623e3961&mode=json', true)
+request.open('GET', 'https://iot.fhh-infra.de/api/v1/tags/parksensoren-boenischplatz/devices?auth=2885232b36725f9cff71f8d80a1b584a', true)
 
 request.onload = function () {
 // Begin accessing JSON data here
